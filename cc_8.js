c7adf7598e725863addb8df39cae5b7c029a3db2
Jbarb25 <barboza205@usf.edu>
2025-02-11 16:41:03 -0500

    Task 2 - Product Price After Discount.
    Write a function that returns the final price based on the parameters that are plugged in when called.

--- cc_8.js
+++ cc_8.js
@@ -1,8 +1,18 @@
 //Task 1: Function Declaration
 function calculateSalary(baseSalary, bonus, taxRate){   //Write a function that computes the net salary
     netSalary = (baseSalary + bonus) - (baseSalary * taxRate);
-    return netSalary}  //Return the net salary to where it is called after calculating the value 
+    return netSalary;}  //Return the net salary to where it is called after calculating the value 
 
 //Call the function to plug in the values for the parameters and calculate the net salary
 console.log(`Net Salary: $${calculateSalary(5000, 500, 0.1).toFixed(2)}`)
 console.log(`Net Salary: $${calculateSalary(7000, 1000, 0.15).toFixed(2)}`)
+
+
+//Task 2: Function Expression
+function calculateDiscount(price, discountRate){  //Write a function to calculate the final price
+    finalPrice = price - (price * discountRate);
+    return finalPrice;} //Return the final price after removing the discount to where it is called
+
+//Call the function to plug in the values for the parameters and calculate the final price
+console.log(`Final Price: $${calculateDiscount(100, 0.2).toFixed(2)}`)
+console.log(`Final Price: $${calculateDiscount(250, 0.15).toFixed(2)}`)
