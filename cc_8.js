73ad032b5c0558e4d6fdbb4a31f853704b439d67
Jbarb25 <barboza205@usf.edu>
2025-02-11 17:24:01 -0500

    Task 4 - Car Rental Cost Calculation
    Write a function to calculate the total rental cost based on the parameters plugged in when calling the function. The function applies a specific calculation based on what the parameters are during the if-else statements.

--- cc_8.js
+++ cc_8.js
@@ -18,7 +18,7 @@ console.log(`Final Price: $${calculateDiscount(100, 0.2).toFixed(2)}`)
 console.log(`Final Price: $${calculateDiscount(250, 0.15).toFixed(2)}`)
 
 
-//Taks 3: Arrow Function
+//Task 3: Arrow Function
 const calculateServiceFee = (amount, serviceType) => {   //Write an arrow function that calculates the service fee based on the parameters
     if (serviceType === "Premium") amount = amount*0.15;
     else if (serviceType === "Standard") amount = amount*0.10;
@@ -28,3 +28,19 @@ const calculateServiceFee = (amount, serviceType) => {   //Write an arrow functi
 //Call the function to plug in the values for the parameters to calculate the service fee
 console.log(`Service Fee: $${calculateServiceFee(200, "Premium").toFixed(2)}`)
 console.log(`Service Fee: $${calculateServiceFee(500, "Standard").toFixed(2)}`)
+
+
+//Task 4: Parameters and Arguments
+function calculateRentalCost(days, carType, insurance = false) {
+    let rentalCost = 0;  
+    if (carType === "Economy") rentalCost = (40*days);
+    else if (carType === "Standard") rentalCost = (60*days);
+    else if (carType === "Luxury") rentalCost = (100*days);
+    else;   //The if-else function filters through the parameters and applies the calculation for the rental cost
+    if (insurance) rentalCost += (20*days);
+    else rentalCost;  //The if-else function will add $20 if there is insurance
+    return rentalCost}
+
+//Call the function to plug in the parameters as the if-else statments filter to apply the right calculation
+console.log(`Total Rental Cost: $${calculateRentalCost(3, "Economy", true)}`)
+console.log(`Total Rental Cost: $${calculateRentalCost(5, "Luxury", false)}`)
